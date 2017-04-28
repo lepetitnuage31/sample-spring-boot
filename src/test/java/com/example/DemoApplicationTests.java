a3670ac953bea6331e4b607bad07606df6a1288b
lepetitnuage31 <joel.barthez@meteo.fr>
2017-04-28 14:39:18 +0200

    fail test

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +12,7 @@ public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		fail("not yet impemented");
 	}
 
 }
